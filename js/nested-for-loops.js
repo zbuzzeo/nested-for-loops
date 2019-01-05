72db268a33acd9554badeedfc00dbb75852b21b6
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-04 15:48:35 -1000

    final

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -6,30 +6,21 @@
  */
 function nestedForLoops( depth, width = depth ) {
     let result = "\n";
-    let x = 0;
-    let y = 0;
-    console.log( `depth: ${ depth }\nwidth: ${ width }` );
+    // Error handling:
+    if ( !depth || isNaN( depth ) ) {
+        return '';
+    }
     for ( let i = 0; i < depth; i++ ) {
         for ( let j = 0; j < width; j++ ) {
-            x = j;
-            y = i;
-            result += `{x:${ x }, y:${ y }}`;
+            result += `{x:${ j }, y:${ i }}`;
             if ( j + 1 < width ) {
                 result += ', ';
             } else {
                 result += '\n';
             }
-            console.log( `%cx: ${ x }, y: ${ y }`, 'color: cornflowerblue; font-size: 1.2em;' );
         }
     }
-    console.log(`FINISHED`);
-    console.log( `%cRESULT: ${ result }`, 'color: orange; font-size: 1.2em;' );
-
-    // Error handling:
-    if ( !depth || isNaN( depth ) ) {
-        return '';
-    } else { return result };
-
+    return result;
 };
 
 // To see your console output outside the tests add function calls here.
